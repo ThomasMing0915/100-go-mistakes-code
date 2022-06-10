7c4a98221b490b80327032db4846ad3dd8d74720
ThomasMing0915 <mingyong@email.szu.edu.cn>
2022-06-10 21:54:26 +0800

    add #64

---
+++ 64/example1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func receiver(messageCh <-chan int, disconnectCh <-chan struct{}, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+
+	for {
+		select {
+		case v := <-messageCh:
+			fmt.Println(v)
+		case <-disconnectCh:
+			fmt.Println("disconnect, return")
+			return
+		}
+	}
+}
+
+func sender(messageCh chan<- int, disconnectCh chan<- struct{}, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+
+	for i := 0; i < 10; i++ {
+		messageCh <- i
+	}
+	disconnectCh <- struct{}{}
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	disconnectCh := make(chan struct{})
+	messageCh := make(chan int, 5)
+
+	wg.Add(2)
+
+	go sender(messageCh, disconnectCh, wg)
+
+	go receiver(messageCh, disconnectCh, wg)
+
+	wg.Wait()
+}
